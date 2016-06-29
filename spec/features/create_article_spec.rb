45d2085c06f3359ad6b4465fe2fd7d197f04beaa
natron19 <nathan19@hotmail.com>
2016-06-29 14:09:08 -0700

    adds validation to article

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -18,4 +18,21 @@ feature "creating article" do
 
   end
 
+  scenario "A use fails to create a new article" do
+
+    visit "/"
+
+    click_link "New Article"
+
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+
+  end
+
 end
