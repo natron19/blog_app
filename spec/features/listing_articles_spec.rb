a7ea5869be6eac4030537dbae32f3eaa14030925
natron19 <nathan19@hotmail.com>
2016-06-29 14:29:09 -0700

    adds listing of articles

---
+++ spec/features/listing_articles_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "Listing Aritcles" do
+
+  before do
+    @article1 = Article.create(title: "First article", body: "body of first article")
+    @article2 = Article.create(title: "Second article", body: "body of second article")
+
+  end
+
+  scenario "list all articles" do
+
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
+
+end
